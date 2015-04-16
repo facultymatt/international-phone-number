ee16a6d9afa235b455998381aad269f7357f4e97
Matt Miller <matt@superpedestrian.com>
2015-04-16 14:21:02 -0400

    update release files with latest src.

--- releases/international-phone-number.js
+++ releases/international-phone-number.js
@@ -1,78 +1,91 @@
-// Author Marek Pietrucha
-// https://github.com/mareczek/international-phone-number
-
-(function() {
-  "use strict";
-  angular.module("internationalPhoneNumber", []).directive('internationalPhoneNumber', function() {
-    return {
-      restrict: 'A',
-      require: '^ngModel',
-      scope: true,
-      link: function(scope, element, attrs, ctrl) {
-        var handleWhatsSupposedToBeAnArray, options, read;
-        read = function() {
-          return ctrl.$setViewValue(element.val());
-        };
-        handleWhatsSupposedToBeAnArray = function(value) {
-          if (typeof value === "object") {
-            return value;
-          } else {
-            return value.toString().replace(/[ ]/g, '').split(',');
-          }
-        };
-        options = {
-          autoFormat: true,
-          autoHideDialCode: true,
-          defaultCountry: '',
-          nationalMode: false,
-          numberType: '',
-          onlyCountries: void 0,
-          preferredCountries: ['us', 'gb'],
-          responsiveDropdown: false,
-          utilsScript: ""
-        };
-        angular.forEach(options, function(value, key) {
-          var option;
-          option = eval("attrs." + key);
-          if (angular.isDefined(option)) {
-            if (key === 'preferredCountries') {
-              return options.preferredCountries = handleWhatsSupposedToBeAnArray(option);
-            } else if (key === 'onlyCountries') {
-              return options.onlyCountries = handleWhatsSupposedToBeAnArray(option);
-            } else if (typeof value === "boolean") {
-              return options[key] = option === "true";
-            } else {
-              return options[key] = option;
-            }
-          }
-        });
-        element.intlTelInput(options);
-        if (!options.utilsScript) {
-          element.intlTelInput('loadUtils', 'bower_components/intl-tel-input/lib/libphonenumber/build/utils.js');
-        }
-        ctrl.$parsers.push(function(value) {
-          if (!value) {
-            return value;
-          }
-          return value.replace(/[^\d]/g, '');
-        });
-        ctrl.$parsers.push(function(value) {
-          if (value) {
-            ctrl.$setValidity('international-phone-number', element.intlTelInput("isValidNumber"));
-          } else {
-            value = '';
-            delete ctrl.$error['international-phone-number'];
-          }
+"use strict";
+angular.module("internationalPhoneNumber", []).directive('internationalPhoneNumber', function() {
+  return {
+    restrict: 'A',
+    require: '^ngModel',
+    scope: {
+      ngModel: '=',
+      defaultCountry: '@'
+    },
+    link: function(scope, element, attrs, ctrl) {
+      var handleWhatsSupposedToBeAnArray, options, read, watchOnce;
+      read = function() {
+        return ctrl.$setViewValue(element.val());
+      };
+      handleWhatsSupposedToBeAnArray = function(value) {
+        if (value instanceof Array) {
           return value;
+        } else {
+          return value.toString().replace(/[ ]/g, '').split(',');
+        }
+      };
+      options = {
+        autoFormat: true,
+        autoHideDialCode: true,
+        defaultCountry: '',
+        nationalMode: false,
+        numberType: '',
+        onlyCountries: void 0,
+        preferredCountries: ['us', 'gb'],
+        responsiveDropdown: false,
+        utilsScript: ""
+      };
+      angular.forEach(options, function(value, key) {
+        var option;
+        if (!(attrs.hasOwnProperty(key) && angular.isDefined(attrs[key]))) {
+          return;
+        }
+        option = attrs[key];
+        if (key === 'preferredCountries') {
+          return options.preferredCountries = handleWhatsSupposedToBeAnArray(option);
+        } else if (key === 'onlyCountries') {
+          return options.onlyCountries = handleWhatsSupposedToBeAnArray(option);
+        } else if (typeof value === "boolean") {
+          return options[key] = option === "true";
+        } else {
+          return options[key] = option;
+        }
+      });
+      watchOnce = scope.$watch('ngModel', function(newValue) {
+        return scope.$$postDigest(function() {
+          options.defaultCountry = scope.defaultCountry;
+          if (newValue !== null && newValue !== void 0 && newValue !== '') {
+            element.val(newValue);
+          }
+          element.intlTelInput(options);
+          if (!(attrs.skipUtilScriptDownload !== void 0 || options.utilsScript)) {
+            element.intlTelInput('loadUtils', '/bower_components/intl-tel-input/lib/libphonenumber/build/utils.js');
+          }
+          return watchOnce();
         });
-        element.on('blur keyup change', function(event) {
-          return scope.$apply(read);
-        });
-        return element.on('$destroy', function() {
-          return element.off('blur keyup change');
-        });
-      }
-    };
-  });
+      });
+      ctrl.$parsers.push(function(value) {
+        if (!value) {
+          return value;
+        }
+        return value.replace(/[^\d]/g, '');
+      });
+      ctrl.$parsers.push(function(value) {
+        var validity;
+        if (value) {
+          validity = element.intlTelInput("isValidNumber");
+          ctrl.$setValidity('international-phone-number', validity);
+          ctrl.$setValidity('', validity);
+        } else {
+          value = '';
+          delete ctrl.$error['international-phone-number'];
+        }
+        return value;
+      });
+      element.on('blur keyup change', function(event) {
+        return scope.$apply(read);
+      });
+      return element.on('$destroy', function() {
+        return element.off('blur keyup change');
+      });
+    }
+  };
+});
 
-}).call(this);
+// ---
+// generated by coffee-script 1.9.0
